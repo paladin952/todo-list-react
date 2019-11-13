a917675707e93226363bf499eb919166c4004196
Clapa Lucian <clapalucian10@gmail.com>
2019-11-13 17:41:26 +0200

    add circular progress

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {VerticalContainer, VerticalMargin} from "../components/general/Generic";
 import styled from "styled-components";
 import TodoInput from "../components/list/TodoInput";
 import TodoList from "../components/list/TodoList";
+import ApiService from "../services/ApiService";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Container = styled(VerticalContainer)`
   width: 767px;
@@ -19,46 +21,62 @@ const Title = styled.h1`
 `;
 
 const MainPage = () => {
-    const [data, setData] = useState(["Pay the bills", "Meat Brian on 7th", "Walk the dog", "Buy milk"]);
-    const [completedIndexes, setCompletedIndexes] = useState([0]);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [completedIndexes, setCompletedIndexes] = useState([]);
+
+    const loadData = async () => {
+        setLoading(true);
+        const data = await ApiService.getData();
+        setData(data);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        loadData();
+    }, []);
 
     return (
         <Container>
-            <Title>Todo List</Title>
+            {loading ? <CircularProgress/>
+                : <div>
+                    <Title>Todo List</Title>
 
-            <TodoInput
-                onSelected={newVal => {
-                    if (newVal) {
-                        setData([...data, newVal])
-                    }
-                }}
-            />
+                    <TodoInput
+                        onSelected={newVal => {
+                            if (newVal) {
+                                setData([...data, newVal])
+                            }
+                        }}
+                    />
 
-            <VerticalMargin/>
+                    <VerticalMargin/>
 
-            <TodoList
-                data={data}
-                completedIndexes={completedIndexes}
-                onRemove={(index) => {
-                    const newList = [...data];
-                    if (index > -1 && index < data.length) {
-                        newList.splice(index, 1);
-                        setData(newList);
-                    }
-                }}
-                onComplete={i => {
-                    const indexOf = completedIndexes.indexOf(i);
-                    const completed = indexOf !== -1;
-                    if (completed) {
-                        const newIndexes = [...completedIndexes];
-                        newIndexes.splice(indexOf, 1);
-                        setCompletedIndexes(newIndexes);
+                    <TodoList
+                        data={data}
+                        completedIndexes={completedIndexes}
+                        onRemove={(index) => {
+                            const newList = [...data];
+                            if (index > -1 && index < data.length) {
+                                newList.splice(index, 1);
+                                setData(newList);
+                            }
+                        }}
+                        onComplete={i => {
+                            const indexOf = completedIndexes.indexOf(i);
+                            const completed = indexOf !== -1;
+                            if (completed) {
+                                const newIndexes = [...completedIndexes];
+                                newIndexes.splice(indexOf, 1);
+                                setCompletedIndexes(newIndexes);
 
-                    } else {
-                        setCompletedIndexes([...completedIndexes, i]);
-                    }
-                }}
-            />
+                            } else {
+                                setCompletedIndexes([...completedIndexes, i]);
+                            }
+                        }}
+                    />
+                </div>
+            }
         </Container>
     );
 };
---
+++ src/services/ApiService.js
@@ -0,0 +1,11 @@
+
+
+export default class ApiService {
+
+    static async getData() {
+
+        return ["Pay the bills", "Meat Brian on 7th", "Walk the dog", "Buy milk"];
+
+    }
+
+}
